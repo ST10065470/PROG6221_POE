0977e779ba0945035d13d2e00841b30d2a76f302
EthanSchoonbee <schoonbeeethan@gmail.com>
2023-03-23 08:42:48 +0200

    Created Recipe Class

---
+++ PROG6221_POE/Recipe.cs
@@ -0,0 +1,16 @@
+﻿//Ethan Schoonbee ST10036509 POE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POE
+{
+    internal class Recipe
+    {
+
+
+
+    }
+}
